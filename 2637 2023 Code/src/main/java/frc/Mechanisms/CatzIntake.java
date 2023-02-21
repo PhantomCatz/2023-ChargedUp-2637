5e62f74237131b7de37238c91a63ded76b9d8f89
Lorennnnnn <120277498+Lorennnnnn@users.noreply.github.com>
2023-02-21 11:05:38 -0800

    Add files via upload

--- 2637 2023 Code/src/main/java/frc/Mechanisms/CatzIntake.java
+++ 2637 2023 Code/src/main/java/frc/Mechanisms/CatzIntake.java
@@ -3,225 +3,254 @@ package frc.Mechanisms;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.Timer;
 
+ 
 public class CatzIntake {
-  public CatzIntake Intake;
-  private Thread intakeThread;
-  //Ritio need correct
-  final double INTAKE_MOTOR_GEAR_RATIO = 1.0/49.0;
 
+  private Thread intakeThread;
+  
 //-----------------------------------------------------------------------------------------------------------
 //Roller
 //-----------------------------------------------------------------------------------------------------------
-  private CANSparkMax intakeRoller;
-  private final int INTAKE_ROLLER_MC_ID        = 30; 
+  //private  VictorSPX intakeRoller;
+  private WPI_TalonFX intakeRollerMotor;
+  //correction needed
+  private final int INTAKE_ROLLER_MC_ID        = 11; 
 
-  private final double INTAKE_ROLLER_MOTOR_POWER       = 0.78;;
-  private final double OUTTAKE_ROLLER_MOTOR_POWER      = 1.0;
-  public  final double AUTON_INTAKE_ROLLER_MOTOR_POWER = 0.8;
+  private final double INTAKE_ROLLER_MOTOR_POWER       = 0.5;
+  private final double OUTTAKE_ROLLER_MOTOR_POWER      = 0.5;
   private final double INTAKE_MOTOR_POWER_OFF          = 0.0;
 
- 
-  public boolean intakeRollerStartIn = false;
-  public boolean intakeRollerOut = false;
- 
+  public final int INTAKE_ROLLER_OFF = 0;
+  public final int INTAKE_ROLLER_IN  = 1;
+  public final int INTAKE_ROLLER_OUT = 2;
+  public final int INTAKE_ROLLER_UNINITIALIZED = -999;
+  public int   intakeRollerState = INTAKE_ROLLER_OFF;
+
 //-----------------------------------------------------------------------------------------------------------
 //Pivot
 //-----------------------------------------------------------------------------------------------------------
-  private WPI_TalonFX deploymentMotor;
-  private final int INTAKE_PIVOT_MC_ID        = 32; 
+  private WPI_TalonFX intakePivotMotor;
+  private final int INTAKE_PIVOT_MC_ID        = 10; 
 
-  private static final int INTAKE_STOW_MINIMUM_ANGLE          = 80;
-  private static final int INTAKE_STOW_MAXIMUM_ANGLE          = 92;
-  private static final int INTAKE_STOW_REDUCE_ANGLE           = 70;
-  private static final int INTAKE_STOW_FULLY_STOWED_ANGLE = 88;
-  private static final int INTAKE_DEPLOY_MINIMUM_ANGLE        = 45;
-  private static final int INTAKE_DEPLOY_REDUCE_ANGLE         = 45;
-  private static final int INTAKE_DEPLOY_FULLY_DEPLOYED_ANGLE = 2;
+  private final int INTAKE_PIVOT_FULLY_DEPLOYED_ANGLE = 90;
+
+  private final double INTAKE_PIVOT_PINION_GEAR = 11.0;
+  private final double INTAKE_PIVOT_SPUR_GEAR   = 56.0;  
+  private final double INTAKE_PIVOT_GEAR_RATIO  =INTAKE_PIVOT_SPUR_GEAR/INTAKE_PIVOT_PINION_GEAR;
+
+  private final double INTAKE_PIVOT_SPROCKET_1  = 16.0;
+  private final double INTAKE_PIVOT_SPROCKET_2  = 56.0;
+  private final double INTAKE_PIVOT_SPROCKET_RATIO  = INTAKE_PIVOT_SPROCKET_2/INTAKE_PIVOT_SPROCKET_1;
   
+  private final double INTAKE_PIVOT_FINAL_RATIO = INTAKE_PIVOT_GEAR_RATIO*INTAKE_PIVOT_SPROCKET_RATIO;
 
-  //Pivot Stow Angle
-  private static final int INTAKE_STOW_RANGE_ACCEPTABLE_ANGLE = 2;
 
-  //Pivot Motor Power For IntakeControl
-  private final double INTAKE_MOTOR_POWER_START_DEPLOY    =  0.4;
-  private final double INTAKE_MOTOR_POWER_REDUCE_DEPLOY   =  0.2;
-  private final double INTAKE_MOTOR_POWER_ADJUST_DEPLOY   =  0.15;
-  private final double INTAKE_MOTOR_POWER_START_STOW      = -0.40;
-  private final double INTAKE_MOTOR_POWER_REDUCE_STOW     = -0.25;
-  private final double INTAKE_MOTOR_POWER_ADJUST_STOW     =  0.15;
-  private final double INTAKE_MOTOR_POWER_STOP     =  0.0;
-  //Pivot Motor Power For Method
-  private final double DEPLOY_MOTOR_POWER              = 0.8;
-  private final double DEPLOY_MOTOR_ADJUST_POWER       = 0.3;
+
 
   //Pivot status
-  private final int INTAKE_MODE_NULL                = 0;
-  public  final int INTAKE_MODE_DEPLOY_START        = 1;
-  private final int INTAKE_MODE_DEPLOY_REDUCE_POWER = 2;
-  private final int INTAKE_MODE_DEPLOY_HOLD         = 3;
-  public  final int INTAKE_MODE_STOW_START          = 4;
-  private final int INTAKE_MODE_STOW_REDUCE_POWER   = 5;
-  private final int INTAKE_MODE_STOW_HOLD           = 6;
+  public  final int INTAKE_MODE_NULL                = 0;
+  public  final int INTAKE_MODE_DEPLOY              = 1;
+  public  final int INTAKE_MODE_INITIALIZATION      = 2;
+  public  final int INTAKE_MODE_STOW                = 3;
+  public  final int INTAKE_MODE_STOW_HOLD           = 4;
 
 //Pivot IntakeMode initialization
   public int intakeMode = INTAKE_MODE_NULL;
 
   public boolean intakeStowed = true;
   public boolean intakeDeployed = false;
+
+  public int counter=0;
+
+  public final int INTAKE_PIVOT_STOWED = 0;
+  public final int INTAKE_PIVOT_DEPLOYED = 1;
+  public final int INTAKE_PIVOT_IN_TRANSIT = 2;
+  public final int INTAKE_PIVOT_UNINITIALIZED = -999;
+  public int intakePivotState = INTAKE_PIVOT_STOWED;
+
+  public Timer pivotTimer;
+
+  public static double time=Timer.getFPGATimestamp();
+
+  public static double finalMotorPower = 0;
+  public static double Kp = 0.01;
+  public static double Kd = 0.001;
+  public final int INTAKE_DEPLOY_FINAL_ANGLE = 90;
+  public final int INTAKE_DEPLOY_INITIAL_ANGLE = 0;
+  public final int INTAKE_STOW_FINAL_ANGLE = 0;
+  public final int INTAKE_STOW_INITIAL_ANGLE = 90;
+  public final double INTAKE_DEPLOYMENT_TIME = 0.26;
+  public static double Ot=0;
+  public static double OtDot = 0;
+  public static double deltaO = 0;
+
+  public static double a3;
+  public static double a4;
+  public static double a5;
+  public static double angleDot = 0;
+  public static double angleOld = 0;
+  public static double currentAngle = 0;
+  public static double timeOld = 0;
+  public static double deltaTime = 0;
+  public static double b=0;
+
+  public static double powerForMotor = 5.24 * (time *time *time) - 30.24 *(time *time *time*time) + 46.52 *(time *time *time *time *time);
+
+
 //---------------------------------------------definition part end--------------------------------------------------------------
   
   public CatzIntake() {
-    Intake = new CatzIntake();
-    intakeRoller    = new CANSparkMax(INTAKE_ROLLER_MC_ID,MotorType.kBrushed);
-    deploymentMotor = new WPI_TalonFX(INTAKE_PIVOT_MC_ID);
+    //need add softlimits
 
-    intakeRoller.restoreFactoryDefaults();
-    deploymentMotor.configFactoryDefault();
+    intakeRollerMotor    = new WPI_TalonFX(INTAKE_ROLLER_MC_ID);
+    intakePivotMotor = new WPI_TalonFX(INTAKE_PIVOT_MC_ID);
 
-    intakeRoller.setIdleMode(IdleMode.kCoast);
-    deploymentMotor.setNeutralMode(NeutralMode.Brake);
-    
+    intakeRollerMotor.configFactoryDefault();
+    intakePivotMotor.configFactoryDefault();
+
+    // intakeRoller.setSomething?current limit
+    intakeRollerMotor.setNeutralMode(NeutralMode.Coast);
+
+    //Timer pivotTimer = new Timer();
+
+   
+   
+
+   //initialize for pivot motor
+   //sensor-->0;
+    intakePivotMotor.setNeutralMode(NeutralMode.Brake);
+
+  
 
   }
+
 
 //---------------------------------------------------Roller--------------------------------------------------------
    
 
-  public void intakeRollerStartIn()
+  public void intakeRollerIn()
     {
-        intakeRoller.set(INTAKE_ROLLER_MOTOR_POWER);
-        intakeRollerStartIn = true;
+        intakeRollerMotor.set(ControlMode.PercentOutput,-INTAKE_ROLLER_MOTOR_POWER);
+        intakeRollerState = INTAKE_ROLLER_IN;
     }
 
     public void intakeRollerOut()
     {
-        intakeRoller.set(-OUTTAKE_ROLLER_MOTOR_POWER);
-        intakeRollerOut = true;
+        intakeRollerMotor.set(ControlMode.PercentOutput,OUTTAKE_ROLLER_MOTOR_POWER);
+        intakeRollerState = INTAKE_ROLLER_OUT;
     }
 
 
     public void intakeRollerOff()
     {
-        intakeRoller.set(INTAKE_MOTOR_POWER_OFF);
-        intakeRollerStartIn = false;
-        intakeRollerOut = false;
+        intakeRollerMotor.set(ControlMode.PercentOutput,INTAKE_MOTOR_POWER_OFF);
+        intakeRollerState = INTAKE_ROLLER_OFF;
     }
+
 
 //------------------------------------------------Pivot--------------------------------------------------------------
-
-    public void deployIn(){
-        deploymentMotor.set(ControlMode.PercentOutput, DEPLOY_MOTOR_POWER);
-    }
-
-
-    public void deployOut(){
-        deploymentMotor.set(ControlMode.PercentOutput, -DEPLOY_MOTOR_POWER);
-    }
-
-    public void deploymentAdjust(){
-        deploymentMotor.set(ControlMode.PercentOutput, -DEPLOY_MOTOR_ADJUST_POWER);
-    }
-
-    //assume it's 0 degrees horizontally and 90 degrees vertically
+    
     public void intakeControl(){
     
         intakeThread = new Thread(() ->
         { 
-    
             while(true)
             {
-                double currentAngle = getIntakeDeployPositionDegrees();
+                //int currentAngle = (int)getIntakeDeployPositionDegrees();
                 switch(intakeMode)
                 {
-
-                    case INTAKE_MODE_DEPLOY_START:
-                        currentAngle = getIntakeDeployPositionDegrees();
-                        deploymentMotor.set(INTAKE_MOTOR_POWER_START_DEPLOY);
-                        if(currentAngle < INTAKE_DEPLOY_REDUCE_ANGLE)
-                        {
-                            intakeMode = INTAKE_MODE_DEPLOY_REDUCE_POWER;
+                    case INTAKE_MODE_DEPLOY:
+                        if(counter ==0 ){
+                        pivotTimer.reset();
                         }
-                        intakeDeployed = true;
-                    break;
-
-                    case INTAKE_MODE_DEPLOY_REDUCE_POWER:
-                        currentAngle = getIntakeDeployPositionDegrees();
-                            deploymentMotor.set(INTAKE_MOTOR_POWER_REDUCE_DEPLOY);
-                            intakeDeployed = true;
-                            Intake.intakeRollerStartIn();
-                            if(currentAngle <= INTAKE_DEPLOY_FULLY_DEPLOYED_ANGLE){
-                                deploymentMotor.set(INTAKE_MOTOR_POWER_OFF);
-                                intakeMode = INTAKE_MODE_DEPLOY_HOLD;
-                            }
                         
-                    break;
-
-                    case INTAKE_MODE_DEPLOY_HOLD:
+                        counter++;
+                        
                         currentAngle = getIntakeDeployPositionDegrees();
-                        if(currentAngle > INTAKE_DEPLOY_MINIMUM_ANGLE)
-                        {
-                        deploymentMotor.set(INTAKE_MOTOR_POWER_ADJUST_DEPLOY);
-                        }
+                        time = pivotTimer.get();
+
+                        deltaO = currentAngle - angleOld;
+                        deltaTime = time - timeOld;
+                        angleOld = getIntakeDeployPositionDegrees();
+                        timeOld = pivotTimer.get();
+
+                        angleDot = deltaO/deltaTime;
+                        
+                        
+                        b = (INTAKE_DEPLOY_FINAL_ANGLE-INTAKE_DEPLOY_INITIAL_ANGLE)/INTAKE_DEPLOYMENT_TIME;
+                       
+                        powerForMotor = 5.24 * (time *time *time) - 30.24 *(time *time *time*time) + 46.52 *(time *time *time *time *time);
+                        a3 = 10*b/INTAKE_DEPLOYMENT_TIME/INTAKE_DEPLOYMENT_TIME;
+                        a4 = -15*b/INTAKE_DEPLOYMENT_TIME/INTAKE_DEPLOYMENT_TIME/INTAKE_DEPLOYMENT_TIME;
+                        a5 = 6*b/INTAKE_DEPLOYMENT_TIME/INTAKE_DEPLOYMENT_TIME/INTAKE_DEPLOYMENT_TIME/INTAKE_DEPLOYMENT_TIME;
+                        Ot = (a3*time*time*time) + (a4*time*time*time*time) + (a5*time*time*time*time*time);
+                        OtDot = (3 * a3 * time * time) + (4 * a4 * time *time * time) + (5 * a5 * time * time * time * time);
+                        finalMotorPower = powerForMotor + Kp*(Ot - getIntakeDeployPositionDegrees()) + Kd*(OtDot - deltaO); 
+                        intakePivotMotor.set(finalMotorPower);
+                        
                         break;
 
-    
-                    case INTAKE_MODE_STOW_START:
-                        currentAngle = getIntakeDeployPositionDegrees();
-                        Intake.intakeRollerOff();
-                        deploymentMotor.set(INTAKE_MOTOR_POWER_START_STOW);
-                        if(currentAngle > INTAKE_STOW_REDUCE_ANGLE){
-                        intakeMode = INTAKE_MODE_STOW_REDUCE_POWER;
+                    case INTAKE_MODE_STOW:
+                        
+                    if(counter ==0 ){
+                        pivotTimer.reset();
                         }
+                        
+                        counter++;
+                        
+                        currentAngle = getIntakeDeployPositionDegrees();
+                        time = pivotTimer.get();
+
+                        deltaO = currentAngle - angleOld;
+                        deltaTime = time - timeOld;
+                        angleOld = getIntakeDeployPositionDegrees();
+                        timeOld = pivotTimer.get();
+
+                        angleDot = deltaO/deltaTime;
+                        
+                        
+                        b = (INTAKE_STOW_FINAL_ANGLE-INTAKE_STOW_INITIAL_ANGLE)/INTAKE_DEPLOYMENT_TIME;//time need correct
+                       
+                        powerForMotor = 5.24 * (time *time *time) - 30.24 *(time *time *time*time) + 46.52 *(time *time *time *time *time);
+                        a3 = 10*b/INTAKE_DEPLOYMENT_TIME/INTAKE_DEPLOYMENT_TIME;
+                        a4 = -15*b/INTAKE_DEPLOYMENT_TIME/INTAKE_DEPLOYMENT_TIME/INTAKE_DEPLOYMENT_TIME;
+                        a5 = 6*b/INTAKE_DEPLOYMENT_TIME/INTAKE_DEPLOYMENT_TIME/INTAKE_DEPLOYMENT_TIME/INTAKE_DEPLOYMENT_TIME;
+                        Ot = (a3*time*time*time) + (a4*time*time*time*time) + (a5*time*time*time*time*time);
+                        OtDot = (3 * a3 * time * time) + (4 * a4 * time *time * time) + (5 * a5 * time * time * time * time);
+                        finalMotorPower = powerForMotor + Kp*(Ot - getIntakeDeployPositionDegrees()) + Kd*(OtDot - deltaO); 
+                        intakePivotMotor.set(finalMotorPower);
+                       
                     break;
 
-                    case INTAKE_MODE_STOW_REDUCE_POWER:
-                        currentAngle = getIntakeDeployPositionDegrees();
-                            deploymentMotor.set(INTAKE_MOTOR_POWER_REDUCE_STOW);
-                            intakeDeployed = false;
-                            if(currentAngle >= INTAKE_STOW_FULLY_STOWED_ANGLE){
-                                deploymentMotor.set(INTAKE_MOTOR_POWER_OFF);
-                                intakeMode = INTAKE_MODE_STOW_HOLD;
-                            }
-                        
-                    break;
 
                     case INTAKE_MODE_STOW_HOLD:
-                        currentAngle = getIntakeDeployPositionDegrees();
-                        double angleDifference = currentAngle-90;//vertical degree/position
-                        if(currentAngle < INTAKE_STOW_MINIMUM_ANGLE)
-                        {
-                            deploymentMotor.set(-INTAKE_MOTOR_POWER_ADJUST_STOW);
-                        }
-                        else if(currentAngle > INTAKE_STOW_MAXIMUM_ANGLE){
-                        deploymentMotor.set(INTAKE_MOTOR_POWER_ADJUST_STOW);
-                        }
-                        else if(Math.abs(angleDifference)<= INTAKE_STOW_RANGE_ACCEPTABLE_ANGLE){
-                            deploymentMotor.set(INTAKE_MOTOR_POWER_STOP);
-                        }
-                        intakeStowed = true;
+                        
+                       
                     break;
 
                     default:
-                        deploymentMotor.set(0.0);
+                        intakePivotMotor.set(INTAKE_MOTOR_POWER_OFF);
+                        intakeRollerOff();
                     break;
                 }
+                
             }
         });
         intakeThread.start();
     
 }
 
+
+
     public double getIntakeDeployPositionDegrees(){
-        //rewrite needed
-        double deploymentMotorOutput = deploymentMotor.getSelectedSensorPosition();
-        System.out.println(deploymentMotorOutput);
-        return Math.abs(deploymentMotorOutput * INTAKE_MOTOR_GEAR_RATIO * 360.0);
+        double deploymentMotorRawPosition = intakePivotMotor.getSelectedSensorPosition();
+        return ((deploymentMotorRawPosition / 2048) *360 * INTAKE_PIVOT_FINAL_RATIO ) % 360;//deploymentMotorRawPosition 0-4096 is 1 rotation
+        //% count will goes
+        //
     }
 
+  
 }
